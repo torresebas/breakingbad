5f1ef8ceae30f397382cf31448ca60f738bed86f
sebastianto <sebastt.93@gmail.com>
2021-05-10 14:26:19 +1000

    App finished

--- src/App.js
+++ src/App.js
@@ -1,70 +1,38 @@
-import React, {useState, useEffect} from "react";
-import styled from "@emotion/styled";
+import React, { useState, useEffect } from "react";
+import "./styles/App.scss";
 
-import Phrase from './components/Phrase'
-
-const Container = styled.div`
-  display: flex;
-  align-items: center;
-  padding-top: 5rem;
-  flex-direction: column;
-`;
-
-const Button = styled.button`
-  box-shadow: inset 0px 1px 0px 0px #ffffff;
-  background: linear-gradient(to bottom, #f9f9f9 5%, #e9e9e9 100%);
-  background-color: #f9f9f9;
-  border-radius: 6px;
-  border: 1px solid #dcdcdc;
-  display: inline-block;
-  color: #666666;
-  font-family: Arial;
-  font-size: 15px;
-  font-weight: bold;
-  padding: 11px 19px;
-  text-decoration: none;
-  text-shadow: 0px 1px 0px #ffffff;
-  transition: background-color .8s ease;
-
-
-  :hover {
-  cursor: pointer;
-  background-color: #CCDDE2; 
-
-  }
-
-
-`;
+import Phrase from "./components/Phrase";
 
 function App() {
   //state
-  const [phrase, setPhrase] =  useState({})
-
+  const [phrase, setPhrase] = useState({});
 
   const requireToAPI = async () => {
-    const api = await fetch("https://breaking-bad-quotes.herokuapp.com/v1/quotes");
+    const api = await fetch(
+      "https://breaking-bad-quotes.herokuapp.com/v1/quotes"
+    );
 
-    const phrase = await api.json()
+    const phrase = await api.json();
 
     setPhrase(phrase[0]);
-  }  
-
+  };
 
   //loading phrase
   useEffect(() => {
-    requireToAPI()
-  },[])
+    requireToAPI();
+  }, []);
 
   return (
-    <Container>
-      <Phrase
-        phrase={phrase}
-      />
+    <div className="main">
+      <div className="phrase-container">
+        <Phrase phrase={phrase} />
+      </div>
 
-      <Button
-        onClick={requireToAPI}
-      >Get Phrase</Button>
-    </Container>
+      <div className="button-container">
+        <button onClick={requireToAPI} className="btn">Get Phrase</button>
+      </div>
+
+    </div>
   );
 }
 
--- src/components/Phrase.js
+++ src/components/Phrase.js
@@ -1,50 +1,16 @@
-import React from 'react'
-import styled from '@emotion/styled';
+import React from "react";
 
-const ContainerPhrase = styled.div`
-  padding: 3rem;
-  border-radius: .5rem;
-  background-color: #fff;
-  max-width: 800px;
+import "../styles/Phrase.scss" 
 
-  @media(min-width: 995px){
-    margin-top:10rem
-  }
 
-  h1 {
-    font-family: Arial, Helvetica, sans-serif;
-    text-align: center;
-    position: relative;
-    padding-left: 4rem;
-
-    &::before {
-      content: open-quote;
-      font-size: 10rem;
-      color: black;
-      left: -1rem;
-      top: -2rem;
-    }
-  }
-
-  p {
-    font-family: Verdana, Geneva, Thaoma, sans-serif;
-    font-size: 1.4rem;
-    font-weight: blod;
-    text-align: center;
-    color: #666;
-    margin-top:; 2rem
-
-  }
-`
-
-const Phrase = ( {phrase} ) => {
+const Phrase = ({ phrase }) => {
   // if(Object.keys(phrase).length === 0 )return null
   return (
-    <ContainerPhrase>
-    <h1>{phrase.quote}</h1>
-    <p>{phrase.author}</p>
-    </ContainerPhrase>
-    );
-}
- 
+    <div className="phrase-container">
+      <h1 className="quote">{phrase.quote}</h1>
+      <p className="author">{phrase.author}</p>
+    </div>
+  );
+};
+
 export default Phrase;
